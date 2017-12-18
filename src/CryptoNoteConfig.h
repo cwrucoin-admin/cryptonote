a9eeb6813f6d7da2be23d0dd579813ccb14b106b
CWRUCoin Admin <cwrucoin@gmail.com>
2017-12-17 23:09:35 -0500

    Added both seed nodes to config file

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "13.59.251.50:10900"
+  "13.59.251.50:10900",
+  "18.217.33.216:10900",
 };
 
 struct CheckpointData {
